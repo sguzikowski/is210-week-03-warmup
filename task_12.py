023c5cce16861fe306e0c0c92d9d7a4665dd5481
sguzikowski <ximule@live.com>
2015-09-11 09:18:19 +0000

    completed tasks 10-12

---
+++ task_12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Yay math is fun."""
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = 0.1
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
