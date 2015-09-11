96d41a0cbbeece94af2ff4a046da7e2be5f70962
sguzikowski <ximule@live.com>
2015-09-11 09:39:47 +0000

    added tasks 13-15

---
+++ task_14.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Cannot wait for more math at this point."""
+
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+
+INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
